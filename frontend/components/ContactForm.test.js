9f9b8685265d9e5c13d8c5f0a0052b6a55bd2354
Pamir Perdeci <pamirperdeci@gmail.com>
2023-04-09 20:49:11 -0400

    renders the contact form header test works and passes

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -9,9 +9,15 @@ test('renders without errors', () => {
 
 });
 
-// test('renders the contact form header', () => {
+ test('renders the contact form header', () => {
+    render(<ContactForm/>)   
+
+    const headerElement = screen.queryByText(/Contact Form/i);
+    console.log(headerElement)
+    expect(headerElement).toBeInTheDocument();
     
-// });
+
+ });
 
 // test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
 
