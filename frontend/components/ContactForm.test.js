2f6bd548bb09f30353207ba1afa819f7b91e4d15
Pamir Perdeci <pamirperdeci@gmail.com>
2023-04-09 23:20:39 -0400

    test created for expecting the last Name error to pop up if the submit button is clicked without input

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -56,13 +56,31 @@ test('renders ONE error message if user enters a valid first name and last name 
     expect(errorMessages).toHaveLength(1)
 });
 
-// test('renders "email must be a valid email address" if an invalid email is entered', async () => {
+test('renders "email must be a valid email address" if an invalid email is entered', async () => {
+    render(<ContactForm/>)
+    const emailField = screen.getByLabelText(/email*/i);
+    userEvent.type(emailField, "brb")
 
-// });
 
-// test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
+    const errorMessage = await screen.findByText(/email must be a valid email address/i)
+    
 
-// });
+});
+
+test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
+
+    render(<ContactForm/>)
+    const submitButton = screen.getByRole("button")
+    userEvent.click(submitButton);
+
+
+    const errorMessage =  await screen.findByText(/lastName is a required field/i);
+    expect(errorMessage).toBeInTheDocument();
+
+
+
+
+});
 
 // test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
 
